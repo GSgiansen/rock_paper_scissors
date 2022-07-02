64632d8157e4f8050cc31b307ed5d96040126dc7
GianSen <giansenrockz@gmail.com>
2022-07-02 22:46:23 +0800

    Added score tracking and game ends for whoever reach 5 first

--- rps.js
+++ rps.js
@@ -4,26 +4,49 @@ function computerPlay(){
     return lst[a]
 }
 
-function playRound(player,computerPlay){
+function playRound(player,computerPlays){
+    console.log(player,computerPlays)
     player=lowercase(player)
     if (player=="rock"){
-        if (computerPlay=="paper") return ("You Lose! "+ capitalize(computerPlay) +" beats "+capitalize(player))
-        else if (computerPlay=="scissors") return ("You Win! "+capitalize(player) +" beats "+capitalize(computerPlay))
-        else return ("Its a tie!!")
-    }    
-    else if (player=="paper"){
-        if (computerPlay=="scissors") return ("You Lose! "+capitalize(computerPlay) +" beats "+capitalize(player))
-        else if (computerPlay=="rock") return ("You Win! "+capitalize(player) +" beats "+capitalize(computerPlay))
-        else return ("Its a tie!!")
+        if (computerPlays=="paper"){ printtext.innerHTML=("You Lose! "+ capitalize(computerPlays) +" beats "+capitalize(player))
+        computerScoreint+=1
+        computerScore.innerHTML=computerScoreint
     }
-    else if (player=="scissors"){
-        if (computerPlay=="rock") return ("You Lose! "+capitalize(computerPlay) +" beats "+capitalize(player))
-        else if (computerPlay=="paper") return ("You Win! "+capitalize(player) +" beats "+capitalize(computerPlay))
-        else return ("Its a tie!!")
-    }
+        else if (computerPlays=="scissors"){ printtext.innerHTML=("You Win! "+capitalize(player) +" beats "+capitalize(computerPlays))
+        playerScoreint+=1
+        playerScore.innerHTML=playerScoreint
 }
 
-function game(){
+        else printtext.innerHTML=("Its a tie!!")
+    }    
+    else if (player=="paper"){
+        if (computerPlays=="scissors") {printtext.innerHTML=("You Lose! "+capitalize(computerPlays) +" beats "+capitalize(player))
+        computerScoreint+=1
+        computerScore.innerHTML=computerScoreint
+    }
+        else if (computerPlays=="rock") {printtext.innerHTML=("You Win! "+capitalize(player) +" beats "+capitalize(computerPlays))
+        playerScoreint+=1
+        playerScore.innerHTML=playerScoreint
+}
+        else printtext.innerHTML=("Its a tie!!")
+    }
+    else if (player=="scissors"){
+        if (computerPlays=="rock") {printtext.innerHTML=("You Lose! "+capitalize(computerPlays) +" beats "+capitalize(player))
+        computerScoreint+=1
+        computerScore.innerHTML=computerScoreint
+    }
+    
+        else if (computerPlays=="paper") {printtext.innerHTML=("You Win! "+capitalize(player) +" beats "+capitalize(computerPlays))
+        playerScoreint+=1
+        playerScore.innerHTML=playerScoreint
+}
+        else printtext.innerHTML=("Its a tie!!")
+    }
+    if (computerScoreint>=5) printtext.innerHTML=("Computer Wins overall!")
+    else if (playerScoreint>=5) printtext.innerHTML=("Player Wins overall!")
+}
+
+/*function game(){
     let comp=0
     let play=0
     //for (let i=0;i<5;i++){
@@ -39,7 +62,8 @@ function game(){
     else return "Its a major tie!!!!"
     }
 
-console.log(game())
+game()
+*/
 
 function lowercase(word){
     let s=""
@@ -56,10 +80,10 @@ function capitalize(str){
 
     return st
   }
-function buttonpressed(a){
-    console.log(a)
 
-}
+
+
+const results =document.querySelector("#results");
 const container = document.querySelector('#container');
 const rock =document.createElement("button")
 const paper =document.createElement("button")
@@ -68,18 +92,41 @@ const scissors =document.createElement("button")
 rock.classList.add("rock")
 rock.textContent="Rock!"
 rock.value="rock"
-rock.onclick=function() {buttonpressed(rock.value)}
+rock.onclick=function() {playRound(rock.value,computerPlay())}
 container.appendChild(rock)
 
 paper.classList.add("paper")
 paper.textContent="Paper!"
 paper.value="paper"
-paper.onclick=function() {buttonpressed(paper.value)}
+paper.onclick=function() {playRound(paper.value,computerPlay())}
 
 
 scissors.classList.add("scissors")
 scissors.textContent="Scissors!"
 scissors.value="scissors"
-scissors.onclick=function() {buttonpressed(scissors.value)}
+scissors.onclick=function() {playRound(scissors.value,computerPlay())}
 container.appendChild(scissors)
 container.appendChild(paper)
+
+let playerScoreint=0
+let computerScoreint=0
+
+
+const printtext=document.createElement("div")
+const smol1=document.createElement("div")
+const playerScore= document.createElement("div")
+const smol2=document.createElement("div")
+const computerScore=document.createElement("div")
+
+smol1.textContent="Player's score:"
+
+smol2.textContent="Computer's score:"
+
+
+playerScore.textContent="0"
+computerScore.textContent="0"
+results.append(smol1)
+results.append(playerScore)
+results.append(smol2)
+results.append(computerScore)
+results.append(printtext)
